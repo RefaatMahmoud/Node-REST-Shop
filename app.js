3ec6e4a562bec66c4345c167acf1e9c3a0ec758d
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2018-07-30 07:27:47 -0700

    Error Handling

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 
-const productRouter = require('./api/routes/products');
-app.use('/products' , productRouter);
+const productRoute = require('./api/routes/products');
+const orderRoute = require('./api/routes/orders');
 
-const orderRouter = require('./api/routes/orders');
-app.use('/orders' , orderRouter);
 
+//Routes Middleware
+app.use('/products' , productRoute);
+app.use('/orders' , orderRoute);
+
+
+//Handle error
+app.use((req , res , next)=>{
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error , req , res, next)=>{
+  res.json({
+    error : {
+      message : error.message
+    }
+  });
+});
 module.exports = app;
