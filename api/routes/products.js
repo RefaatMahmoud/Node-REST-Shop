10d986e529539b479d6027b27616081b0eeb3190
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2018-08-02 05:28:20 -0700

    Validation Requests and Modify Responses in Product Model

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,36 +5,60 @@ const Product = require("../models/product");
 
 router.get('/', (req, res, next) => {
   Product.find()
-  .exec()
-  .then(result => {
-    res.status('200').json({
-      data : result
-    });
-  })
-  .catch(err => {
-    res.status('500').json({
-      error : err
+    .select('_id name price')
+    .exec()
+    .then(docs => {
+      count = docs.length;
+      //create response
+      const response = {
+        count: count,
+        data: docs.map(doc => {
+          return {
+            _id: doc._id,
+            name: doc.name,
+            price: doc.price,
+            request: {
+              type: "GET",
+              URL: `localhost:3000/products/${doc._id}`
+            }
+          }
+        })
+      }
+      //return response
+      res.status('200').json(response);
     })
-  });
+    .catch(err => {
+      //return error response
+      res.status('500').json({
+        error: err
+      })
+    })
 });
 
 router.post('/', (req, res, next) => {
-
+  //create Object
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
   });
-
   product.save()
-    .then(result => {
-      res.status(201).json({
-        message: "Handle POST requests for new product",
-        createProduct: result
-      });
+    .then(newdoc => {
+      //create response
+      const response = {
+        message: "POST request for new product",
+        data: {
+          _id: newdoc._id,
+          name: newdoc.name,
+          price: newdoc.price
+        }
+      }
+      //return response
+      res.status('201').json(response)
     })
+    //return Error response
     .catch(err => {
-      console.log(err)
+      res.status('500').json(err)
     });
 });
 
@@ -44,14 +68,20 @@ router.get('/:productId', (req, res, next) => {
       _id: id
     })
     .exec()
-    .then(result => {
-      res.status('200').json({
-        data : result
-      });
+    .then(doc => {
+      const response = {
+        message: "GET request for product by id",
+        data: {
+          _id: doc._id,
+          name: doc.name,
+          price: doc.price
+        }
+      }
+      res.status('201').json(response)
     })
     .catch(err => {
-      res.status('500').json({
-        error : err
+      res.status('404').json({
+        error: "No Valid entry found for provide id"
       })
     });
 });
@@ -60,34 +90,51 @@ router.patch('/:productId', (req, res, next) => {
   id = req.params.productId
   newName = req.body.name;
   newPrice = req.body.price;
-  Product.update({_id:id} , {$set:{name:newName , price:newPrice}})
-  .exec()
-  .then(result => {
-    res.status('200').json({
-      data : result
+  Product.update({
+      _id: id
+    }, {
+      $set: {
+        name: newName,
+        price: newPrice
+      }
     })
-  })
-  .catch(err => {
-    res.status('500').json({
-      error : err
+    .exec()
+    .then(doc => {
+      const response = {
+        message: "PATCH request for product by id",
+        data: {
+          _id: id,
+          name: newName,
+          price: newPrice
+        }
+      }
+      res.status('200').json(response)
+    })
+    .catch(err => {
+      res.status('404').json({
+        error: "No Valid entry found for provide id"
+      })
     });
-  });
 });
 
 router.delete('/:productId', (req, res, next) => {
   id = req.params.productId;
-  Product.remove({_id : id})
-  .exec()
-  .then(result => {
-    res.status('200').json({
-      data : "this item is removed successfully"
+  Product.remove({
+      _id: id
+    })
+    .exec()
+    .then(doc => {
+      const response = {
+        message: "DELETE request for product by id",
+        data: "this item is removed successfully"
+      }
+      res.status('200').json(response);
+    })
+    .catch(err => {
+      res.status('404').json({
+        error: err
+      });
     });
-  })
-  .catch(err => {
-    res.status('500').json({
-      error : err
-    });
-  });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -6,34 +6,35 @@ const orderRoute = require('./api/routes/orders');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-
-//mongoose.Promise = global.Promise;
 //Connect DB
 mongoose.connect('mongodb://localhost/node-REST-shop')
-.then(()=>console.log('connected to Mongo db'))
-.catch(err => console.log(err));
+  .then(() => console.log('connected to Mongo db'))
+  .catch(err => console.log(err));
 
+mongoose.Promise = global.Promise;
 //Morgan Middleware
 app.use(morgan('dev'));
 //body-pareser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 //Routes Middleware
-app.use('/products' , productRoute);
-app.use('/orders' , orderRoute);
+app.use('/products', productRoute);
+app.use('/orders', orderRoute);
 
 //Handle error
-app.use((req , res , next)=>{
+app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
 
-app.use((error , req , res, next)=>{
+app.use((error, req, res, next) => {
   res.json({
-    error : {
-      message : error.message
+    error: {
+      message: error.message
     }
   });
 });
